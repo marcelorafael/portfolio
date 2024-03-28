1e39c183ad18914f0d5939661f7b57e09e4c1927
marcelorafael <marcelorafael23@hotmail.com>
2024-03-28 19:56:15 -0300

    feat: base portifolio ok

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -4,25 +4,55 @@ import { Link } from "react-router-dom";
 
 import * as S from './styles'
 
+import background from '../../assets/dark_background.jpg';
+import linkdin from '../../assets/linkedin.png'
+import whatsapp from '../../assets/whatsapp.png'
+import github from '../../assets/github.png'
+
+const linksData = [{ name: 'Início', route: '/' }, { name: 'Sobre', route: 'about' }, { name: 'Meus Projetos', route: 'projects' }]
 
 
 const Home = () => {
     return (
-        <S.Wraper>
+        <S.Wraper
+
+            style={{
+                backgroundImage: `url(${background})`,
+                height: "100vh",
+                marginTop: "-0.5px",
+                backgroundSize: "cover",
+                backgroundRepeat: "no-repeat",
+            }}
+        >
             <S.Header>
-                {[{ name: 'Início' }, { name: 'Sobre' }, { name: 'Meus Projetos' }].map((item: any) => (
+                {linksData.map((item: any) => (
                     <div style={{
                         minWidth: 80
                     }}>
-                        <Link to='/' style={{ textDecoration: 'none', color:'#fff' }}>
+                        <Link to={item.route} style={{ textDecoration: 'none', color: '#fff' }}>
                             <p>{item.name}</p>
                         </Link>
                     </div>
                 ))}
             </S.Header>
-            <S.CenterView>
-                <p>Home</p>
-            </S.CenterView>
+
+            <S.CenterViewName
+            >
+                <S.FirstName>Marcelo</S.FirstName>
+                <S.LastName>Azevedo</S.LastName>
+            </S.CenterViewName>
+
+            <S.Footer>
+                <a href="https://api.whatsapp.com/send/?phone=91982016505&text&type=phone_number&app_absent=0" target="_blank">
+                    <img src={whatsapp} alt="Contato de whatssap" style={{ width: 50, marginRight: 10, marginLeft: 15, cursor: 'pointer' }} />
+                </a>
+                <a href="https://www.linkedin.com/in/marcelorafael23/"  target="_blank">
+                    <img src={linkdin} alt="Contato de Linkein" style={{ width: 50, cursor: 'pointer', marginRight: 10 }} />
+                </a>
+                <a href="https://github.com/marcelorafael"  target="_blank">
+                    <img src={github} alt="Contato de GitHub" style={{ width: 50, cursor: 'pointer' }} />
+                </a>
+            </S.Footer>
         </S.Wraper>
     )
 }
--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -21,6 +21,21 @@ export const CenterView = styled.div`
     `}
 `
 
+export const CenterViewName = styled.div`
+    ${({theme}) => css`
+        height: 80%;
+        width: 80%;
+
+        border-radius: 8px;
+
+        display: flex;
+        flex-direction: column;
+
+        justify-content: center;
+        align-items: flex-start;
+    `}
+`
+
 export const Header = styled.div`
     ${({theme}) => css`
         position: absolute;
@@ -37,6 +52,33 @@ export const Header = styled.div`
 
         align-items: flex-start; */
         padding: 10px;
+    `}
+`;
 
+export const FirstName = styled.p`
+    ${({theme}) => css`
+        color: #fff;
+        font-size: 50px;
+    `}
+`;
+
+export const LastName = styled.p`
+    ${({theme}) => css`
+        color: #fff;
+        font-size: 60px;
+        font-weight: bold;
+    `}
+`;
+export const Footer = styled.div`
+    ${() => css`
+        position: absolute;
+        bottom: 0px;
+
+        width: 100%;
+
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+        padding: 10px;
     `}
 `
